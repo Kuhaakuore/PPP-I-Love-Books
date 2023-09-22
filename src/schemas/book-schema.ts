b3417be3ce34747a2803502c500586667dffe139
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-09-22 17:06:50 -0300

    [REFACTOR] added field cover for books

--- src/schemas/book-schema.ts
+++ src/schemas/book-schema.ts
@@ -2,8 +2,9 @@ import joi from "joi";
 import { CreateBook } from "../protocols/book";
 
 export const bookSchema = joi.object<CreateBook>({
-  title: joi.string().required(),
-  author: joi.string().required(),
-  publisher: joi.string().required(),
-  purchaseDate: joi.date().required()
+  title: joi.string().trim().required(),
+  author: joi.string().trim().required(),
+  publisher: joi.string().trim().required(),
+  purchaseDate: joi.date().required(),
+  cover: joi.string().trim().required()
 });
